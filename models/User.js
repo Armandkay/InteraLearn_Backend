19504c9ff967abf5dd406e4ea568159fbf36ebb5
Armandkay <a.kayiranga1@alustudent.com>
2024-10-30 03:46:14 -0700

    Run

--- models/User.js
+++ models/User.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  userID: String,
-  name: String,
-  email: { type: String, unique: true, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['student', 'instructor', 'admin'], default: 'student' }
+  email: { type: String, required: true, unique: true },
+  role: { type: String, enum: ['student', 'teacher', 'admin'], default: 'student' }
 });
 
 module.exports = mongoose.model('User', userSchema);
